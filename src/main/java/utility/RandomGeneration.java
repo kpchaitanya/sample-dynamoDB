0d3bd8f685ea85e37f0ea3407375277b1ff74cf5
Artem Nikitin <artemnikitin@artemnikitin.com>
2014-03-10 09:26:57 +0300

    Update RandomGeneration.java

--- src/main/java/utility/RandomGeneration.java
+++ src/main/java/utility/RandomGeneration.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class RandomGeneration {
 		
-	public Long randomNumberNotNull(int len) {
-		String AB = "123456789";
-		return Long.parseLong(randomizer(len, AB));
-	}
+    public Long randomNumberNotNull(int len) {
+	String AB = "123456789";
+	return Long.parseLong(randomizer(len, AB));
+    }
 		
-	public float randomFloat(int len) {
-		String AB = "0123456789";
-		return Float.parseFloat("0."+randomizer(len,AB));
-	}
+    public float randomFloat(int len) {
+	String AB = "0123456789";
+	return Float.parseFloat("0."+randomizer(len,AB));
+    }
 
     private String randomizer(int len, String AB) {
         Random rnd = new Random();
